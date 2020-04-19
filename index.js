32c034cfb09e2e5a5aa2fd1020f0315489f42127
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-19 23:27:52 +0530

    Elapsed | Duration

--- index.js
+++ index.js
@@ -29,13 +29,46 @@ video.addEventListener('pause', updatePlayButton);
 
 function updatePlayButton() {
     playpauseIcon.forEach(icon => icon.classList.toggle('hidden'));
-  
+
     if (video.paused) {
-      play.setAttribute('data-title', 'Play (k)')
+        play.setAttribute('data-title', 'Play (k)')
     } else {
-      play.setAttribute('data-title', 'Pause (k)')
+        play.setAttribute('data-title', 'Pause (k)')
     }
 
-  }
+}
 
+
+const timeElapsed = document.getElementById('time-elapsed');
+const duration = document.getElementById('duration');
+
+// formatTime takes a time length in seconds and returns the time in minutes and seconds
+function formatTime(timeInSeconds) {
+    const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+
+    return {
+        minutes: result.substr(3, 2),
+        seconds: result.substr(6, 2),
+    };
+};
+
+// initializeVideo sets the video duration, and maximum value of progress bar
+function initializeVideo() {
+    const videoDuration = Math.round(video.duration);
+    const time = formatTime(videoDuration);
+    duration.innerText = `${time.minutes}:${time.seconds}`;
+    duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+video.addEventListener('loadedmetadata', initializeVideo);
+
+
+//Changes the elapsed duration text after video time has been updated, i.e. after each second.
+function updateTimeElapsed() {
+    const time = formatTime(Math.round(video.currentTime));
+    timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+    timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+video.addEventListener('timeupdate', updateTimeElapsed);
 
