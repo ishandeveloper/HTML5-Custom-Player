e7fcbf52843540b7704552cc9a3f1a5e840fed2e
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-20 00:03:17 +0530

    Volume Icon

--- index.js
+++ index.js
@@ -129,3 +129,23 @@ function updateVolume() {
 }
 volume.addEventListener('input', updateVolume);
 
+
+// Update Volume Controls Icon
+function updateVolumeIcon() {
+    volumeIcons.forEach(icon => {
+      icon.classList.add('hidden');
+    });
+  
+    volumeButton.setAttribute('data-title', 'Mute (m)')
+  
+    if (video.muted || video.volume === 0) {
+      volumeMute.classList.remove('hidden');
+      volumeButton.setAttribute('data-title', 'Unmute (m)')
+    } else if (video.volume > 0 && video.volume <= 0.5) {
+      volumeLow.classList.remove('hidden');
+    } else {
+      volumeHigh.classList.remove('hidden');
+    }
+  }
+
+  video.addEventListener('volumechange', updateVolumeIcon);
