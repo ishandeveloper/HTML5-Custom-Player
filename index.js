39b52e2ab9832790c918a5e77821c4e16dc7c6b3
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-19 23:05:05 +0530

    Video Controls

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const video = document.querySelector('#video');
 const videoControls = document.querySelector('#video-controls');
 
+
+// Checking Video Format Support
 const videoWorks = !!document.createElement('video').canPlayType;
 if (videoWorks) {
-  video.controls = false;
-  videoControls.classList.remove('hidden');
+    video.controls = false;
+    videoControls.classList.remove('hidden');
 }
