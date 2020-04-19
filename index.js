c6dba60c4d1c961a5e550b8e483250246f1cb797
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-19 23:18:51 +0530

    Play|Pause Icon change

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ if (videoWorks) {
     video.controls = false;
     videoControls.classList.remove('hidden');
 }
+
+
+//Play
+const play = document.querySelector('#play');
+play.addEventListener('click', togglePlay);
+
+function togglePlay() {
+    if (video.paused || video.ended) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+//Changing Play Icon To Pause Icon
+const playpauseIcon = document.querySelectorAll('.playback-icons use');
+
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+
+function updatePlayButton() {
+    playpauseIcon.forEach(icon => icon.classList.toggle('hidden'));
+}
